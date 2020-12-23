1f4fc5fa6559185008a6994fb85ce82552fad454
phamhuek91 <61769911+phamhuek91@users.noreply.github.com>
2020-12-23 17:06:30 +0700

    Create vonglapmoi.java

---
+++ vonglapmoi.java
@@ -0,0 +1,18 @@
+package mangdulieu1;
+import java.util.Scanner;
+public class helloworld {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();		
+	    int b = sc.nextInt();
+	    for(int i = a; i<= b; i++ ){
+		    System.out.print(i + " ");
+
+	    }
+	}
+}
+//kết quả
+/*5 6 7 8 9 10 */
+Bài 2: Bạn hãy viết chương trình nhập vào từ bàn phím 
+số nguyên n và thực hiện hiển thị các số từ n về -n.
